f968129593d4e7e88d2130411ccc3d13812b8bf5
simon <SWONG045@e.ntu.edu.sg>
2022-07-01 20:06:52 +0800

    Update

---
+++ btest/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Section from './components/Section';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header/>
+        <Section/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
